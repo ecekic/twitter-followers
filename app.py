e1152d64a26bd482928477b67457134f6c47dd6a
ecekic <64327424+ecekic@users.noreply.github.com>
2021-05-10 20:58:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,6 @@ import csv
 app = Flask(__name__)
 api = Api(app)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'flask'
  
 mysql = MySQL(app)
 
